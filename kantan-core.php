cc893af379b2c9b57673e4194f959f8e95854cd4
xama5 <contact@oliver.la>
2019-10-27 21:07:36 +0100

    Move code out from theme and add a plugin

---
+++ kantan-core.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Plugin Name: Kantan Core
+ * Plugin URI: https://github.com/wp-kantan/kantan-core
+ * GitHub Plugin URI: wp-kantan/kantan-core
+ * Description: Provides core functionality for the Kantan theme
+ * Author: xama5
+ * Version: 1.0.0
+ * Author URI: https://github.com/xama5
+ */
+
+require(__DIR__ . '/src/kantan.php');
